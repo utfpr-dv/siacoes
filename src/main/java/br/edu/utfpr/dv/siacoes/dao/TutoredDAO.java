379dfc4cafa4d4ee754ee23fcd3adf0baa70417a
Newton Carlos <newtoncw@gmail.com>
2018-03-08 17:02:39 -0300

    Correção de erros

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/TutoredDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/TutoredDAO.java
@@ -24,23 +24,27 @@ public class TutoredDAO {
 			String sql = "SELECT idSupervisor, supervisorName, semester, year, SUM(total) AS total FROM(";
 			
 			if((stage == 0) || (stage == 1)) {
-				sql += "SELECT supervisor.idUser AS idSupervisor, supervisor.name AS supervisorName, " + 
+				sql += "SELECT COALESCE(newsupervisor.idUser, supervisor.idUser) AS idSupervisor, COALESCE(newsupervisor.name, supervisor.name) AS supervisorName, " + 
 						"proposal.semester, proposal.year, COUNT(*) AS total " + 
-						"FROM proposal INNER JOIN \"user\" supervisor ON supervisor.idUser=proposal.idSupervisor " + 
+						"FROM proposal INNER JOIN \"user\" supervisor ON supervisor.idUser=proposal.idSupervisor " +
+						"LEFT JOIN project ON project.idProposal=proposal.idProposal " + 
+						"LEFT JOIN \"user\" newsupervisor ON newsupervisor.idUser=project.idSupervisor " +
 						"WHERE proposal.invalidated=0 AND proposal.idDepartment=" + String.valueOf(idDepartment) + 
-						"GROUP BY proposal.year, proposal.semester, supervisor.idUser, supervisor.name";
+						"GROUP BY proposal.year, proposal.semester, COALESCE(newsupervisor.idUser, supervisor.idUser), COALESCE(newsupervisor.name, supervisor.name)";
 			}
 			if(stage == 0) {
 				sql += " UNION ALL ";
 			}
 			if((stage == 0) || (stage == 2)) {
-				sql += "SELECT supervisor.idUser AS idSupervisor, supervisor.name AS supervisorName, " + 
+				sql += "SELECT COALESCE(newsupervisor.idUser, supervisor.idUser) AS idSupervisor, COALESCE(newsupervisor.name, supervisor.name) AS supervisorName, " + 
 						"CASE project.semester WHEN 1 THEN 2 ELSE 1 END AS semester, CASE project.semester WHEN 1 THEN project.year ELSE project.year + 1 END AS year, COUNT(*) AS total " + 
 						"FROM project INNER JOIN \"user\" supervisor ON supervisor.idUser=project.idSupervisor " + 
 						"INNER JOIN proposal ON proposal.idProposal=project.idProposal " +
 						"INNER JOIN finaldocument ON finaldocument.idProject=project.idProject " + 
+						"LEFT JOIN supervisorchange ON supervisorchange.idProposal=proposal.idProposal " + 
+						"LEFT JOIN \"user\" newsupervisor ON newsupervisor.idUser=supervisorchange.idNewSupervisor " +
 						"WHERE finaldocument.supervisorFeedback=1 AND proposal.idDepartment=" + String.valueOf(idDepartment) + 
-						"GROUP BY project.year, project.semester, supervisor.idUser, supervisor.name";
+						"GROUP BY project.year, project.semester, COALESCE(newsupervisor.idUser, supervisor.idUser), COALESCE(newsupervisor.name, supervisor.name)";
 			}
 			
 			sql += ") AS temp WHERE year BETWEEN " + String.valueOf(initialYear) + " AND " + String.valueOf(finalYear) + 
