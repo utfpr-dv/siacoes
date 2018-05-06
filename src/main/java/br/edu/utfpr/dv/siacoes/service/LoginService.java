9aa6274f5367cc2c2d8181aad109605c8fb7c34f
Newton Carlos <newtoncw@gmail.com>
2018-05-06 19:16:16 -0300

    Ajustes REST

--- src/main/java/br/edu/utfpr/dv/siacoes/service/LoginService.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/service/LoginService.java
@@ -18,7 +18,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.google.gson.Gson;
 
 import br.edu.utfpr.dv.siacoes.bo.UserBO;
 import br.edu.utfpr.dv.siacoes.model.Credential;
@@ -30,12 +29,8 @@ public class LoginService {
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response validateLogin(String credentialsJson) {
+	public Response validateLogin(Credential credentials) {
 		try {
-			Gson gson = new Gson();
-
-			Credential credentials = gson.fromJson(credentialsJson, Credential.class);
-
 			UserBO bo = new UserBO();
 			User user = bo.validateLogin(credentials);
 			
