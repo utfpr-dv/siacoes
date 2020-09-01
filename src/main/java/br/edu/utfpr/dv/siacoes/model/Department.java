3187b21ba8652810536a7b7e6bfb67ba7065d0df
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2020-09-01 00:33:47 -0300

    Aplicado Lombok na classe Department com isso deixando o código mais limpo e facilitando a manutenção pois: - KISS: o código é escrito de uma forma mais simples. - Não é necessário verificar se o nome dos setters e getters estão no padrão correto. - Não é necessário verificar se a assinatura dos getters e setters esta correta - YAGNI: não é necessário verificar se existem métodos órfãos (que não são mais utilizados) a cada refatoração do código

--- src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
@@ -1,7 +1,10 @@
 ﻿package br.edu.utfpr.dv.siacoes.model;
 
+import lombok.*;
+
 import java.io.Serializable;
 
+@Data
 public class Department implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -25,58 +28,5 @@ public class Department implements Serializable {
 		this.setSite("");
 		this.setInitials("");
 	}
-	
-	public int getIdDepartment() {
-		return idDepartment;
-	}
-	public void setIdDepartment(int idDepartment) {
-		this.idDepartment = idDepartment;
-	}
-	public Campus getCampus() {
-		return campus;
-	}
-	public void setCampus(Campus campus) {
-		this.campus = campus;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getFullName(){
-		return fullName;
-	}
-	public void setFullName(String fullName){
-		this.fullName = fullName;
-	}
-	public byte[] getLogo() {
-		return logo;
-	}
-	public void setLogo(byte[] logo) {
-		this.logo = logo;
-	}
-	public boolean isActive() {
-		return active;
-	}
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	public String getSite(){
-		return site;
-	}
-	public void setSite(String site){
-		this.site = site;
-	}
-	public String getInitials() {
-		return initials;
-	}
-	public void setInitials(String initials) {
-		this.initials = initials;
-	}
-	
-	public String toString(){
-		return this.getName();
-	}
-	
+
 }
