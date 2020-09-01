811790c2ccec0e74072275f3df46f39aa4a24abf
mayconcamargo <31803811+mayconcamargo@users.noreply.github.com>
2020-09-01 20:41:56 -0300

    Aplicação biblioteca lombok na classe Department
    Neste código foi retirado todo os métodos gets, sets e utilizado a  biblioteca lombok.Data, que diminui o código e mantem os mesmos métodos que foram retirados.

--- src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/Department.java
@@ -1,82 +1,29 @@
-﻿package br.edu.utfpr.dv.siacoes.model;
+package br.edu.utfpr.dv.siacoes.model;
 
 import java.io.Serializable;
+import lombok.Data;
+/*
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+*/
+
+@Data
 public class Department implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private int idDepartment;
-	private Campus campus;
-	private String name;
-	private String fullName;
-	private transient byte[] logo;
-	private boolean active;
-	private String site;
-	private String initials;
+	private int idDepartment= 0;
+	private Campus campus= new Campus();
+	private String name= "";
+	private String fullName= "";
+	private transient byte[] logo= null;
+	private boolean active= true;
+	private String site= "";
+	private String initials="";
 	
-	public Department(){
-		this.setIdDepartment(0);
-		this.setCampus(new Campus());
-		this.setName("");
-		this.setFullName("");
-		this.setLogo(null);
-		this.setActive(true);
-		this.setSite("");
-		this.setInitials("");
-	}
 	
-	public int getIdDepartment() {
-		return idDepartment;
-	}
-	public void setIdDepartment(int idDepartment) {
-		this.idDepartment = idDepartment;
-	}
-	public Campus getCampus() {
-		return campus;
-	}
-	public void setCampus(Campus campus) {
-		this.campus = campus;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getFullName(){
-		return fullName;
-	}
-	public void setFullName(String fullName){
-		this.fullName = fullName;
-	}
-	public byte[] getLogo() {
-		return logo;
-	}
-	public void setLogo(byte[] logo) {
-		this.logo = logo;
-	}
-	public boolean isActive() {
-		return active;
-	}
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	public String getSite(){
-		return site;
-	}
-	public void setSite(String site){
-		this.site = site;
-	}
-	public String getInitials() {
-		return initials;
-	}
-	public void setInitials(String initials) {
-		this.initials = initials;
-	}
-	
-	public String toString(){
-		return this.getName();
-	}
 	
 }
