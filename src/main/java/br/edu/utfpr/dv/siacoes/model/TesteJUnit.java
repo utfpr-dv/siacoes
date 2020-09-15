665ac31048ef2a1b6386331606a80355bf2bf927
mayconcamargo <31803811+mayconcamargo@users.noreply.github.com>
2020-09-15 20:14:18 -0300

    Classe Test JUnit
    Essa classe foi criada para poder realizar todos os testes em conjunto e serão executadas uma por uma, utilizando anotações @RunWith,  @SuiteClasses e @Suite

---
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/TesteJUnit.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.dv.siacoes.model;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TesteJUnitCampus.class, TesteJUnitDepartment.class })
+public class TesteJUnit {
+
+}
