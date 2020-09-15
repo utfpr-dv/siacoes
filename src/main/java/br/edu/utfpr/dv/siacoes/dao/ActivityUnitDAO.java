8129e7c6a4394dbe3ab89c9c6c7b1e25611b696f
mayconcamargo <31803811+mayconcamargo@users.noreply.github.com>
2020-09-15 19:23:58 -0300

    TemplateDao na classe ActivityUnitDAO
    Criado herança da classe TemplateDao na classe ActivityUnitDAO.

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/ActivityUnitDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/ActivityUnitDAO.java
@@ -10,20 +10,22 @@ import java.util.List;
 
 import br.edu.utfpr.dv.siacoes.log.UpdateEvent;
 import br.edu.utfpr.dv.siacoes.model.ActivityUnit;
-//Em primeiro momento foi constatado que o código abaixo, este bem detalhado,
-//fácil entendimento e visualização, porem há muitas validações para fechamento de conexão com banco de dados, então visto isso
-//encontrei uma outra maneira para fazer, utilizando try-with-resources
+
 import br.edu.utfpr.dv.siacoes.model.Department;
 
 
-//código corrigido, criado classe public para sair da conexão, foi uma forma que encontrei para correção do problema
 public class CloseException extends Exception {
-    public CloseException(Throwable e) {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CloseException(Throwable e) {
         super(e);
     }
 }    
 
-public class ActivityUnitDAO {
+public class ActivityUnitDAO extends TemplateDao {
 	
 	
 	
@@ -43,93 +45,11 @@ public class ActivityUnitDAO {
 		
 			
 			return list;
-		}//ao inves de usar o finally ou mesmo chamar o método close(), para fechamento de recursos, 
-			 //usamos apenas catch (SQLException e)
+		}
 		 catch (SQLException e) {
                      throw new CloseException(e);
 		}
-		
-		}
-	
-
-	//criado classe privada chamando a consulta do activity e ao gerar a conexão utilizamos a variavel criada
-         private static final String SQLAct = 
-            "SELECT * FROM activityunit WHERE idActivityUnit=?";
-	
-	public ActivityUnit findById(int id) throws SQLException, CloseException{
-		try (
-		      Connection conn = ConnectionDAO.getInstance().getConnection();
-		      PreparedStatement stmt = conn.prepareStatement(SQLAct);
-	    ){
-		stmt.setInt(1, id);
-		try (ResultSet rs = stmt.executeQuery()){
-			
-			if(rs.next()){
-				return this.loadObject(rs);
-			}else{
-				return null;
-			}}
-		}//ao inves de usar o finally ou mesmo chamar o método close(), para fechamento de recursos, 
-			 //usamos apenas catch (SQLException e)
-		 catch (SQLException e) {
-                     throw new CloseException(e);
-		}
+	  }
 	}
-	
-	public int save(int idUser, ActivityUnit unit) throws SQLException, CloseException{
-		boolean insert = (unit.getIdActivityUnit() == 0);
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			
-			if(insert){
-				stmt = conn.prepareStatement("INSERT INTO activityunit(description, fillAmount, amountDescription) VALUES(?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-			}else{
-				stmt = conn.prepareStatement("UPDATE activityunit SET description=?, fillAmount=?, amountDescription=? WHERE idActivityUnit=?");
-			}
-			
-			stmt.setString(1, unit.getDescription());
-			stmt.setInt(2, (unit.isFillAmount() ? 1 : 0));
-			stmt.setString(3, unit.getAmountDescription());
-			
-			if(!insert){
-				stmt.setInt(4, unit.getIdActivityUnit());
-			}
-			
-			stmt.execute();
-			
-			if(insert){
-				rs = stmt.getGeneratedKeys();
-				
-				if(rs.next()){
-					unit.setIdActivityUnit(rs.getInt(1));
-				}
-				
-				new UpdateEvent(conn).registerInsert(idUser, unit);
-			} else {
-				new UpdateEvent(conn).registerUpdate(idUser, unit);
-			}
-			
-			return unit.getIdActivityUnit();
-		}//ao inves de usar o finally ou mesmo chamar o método close(), para fechamento de recursos, 
-			 //usamos apenas catch (SQLException e)
-		 catch (SQLException e) {
-                     throw new CloseException(e);
-		}
-	}
-	
-	private ActivityUnit loadObject(ResultSet rs) throws SQLException{
-		ActivityUnit unit = new ActivityUnit();
-		
-		unit.setIdActivityUnit(rs.getInt("idActivityUnit"));
-		unit.setDescription(rs.getString("Description"));
-		unit.setFillAmount(rs.getInt("fillAmount") == 1);
-		unit.setAmountDescription(rs.getString("amountDescription"));
-		
-		return unit;
-	}
-
 }
+	
