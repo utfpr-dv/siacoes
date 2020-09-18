5fff83ef4c37340411a073cabda2c4ec02dc77df
HeloisaSoaresUtpr <helo_soarescosta@hotmail.com>
2020-09-18 00:55:55 -0300

    Teste Departament

---
+++ src/main/java/br/edu/utfpr/dv/siacoes/model/DepartmentTest.java
@@ -0,0 +1,20 @@
+package br.edu.utfpr.dv.siacoes.model;
+
+
+import org.junit.Test;
+
+class DepartmentTest {
+
+    private final Department dep = new Department();
+
+    @Test
+    public void teste_classe(){
+        assertEquals(0, dep.idDepartment();
+
+    }
+
+
+
+
+
+}
